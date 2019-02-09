faa7714d0daebb0a0cbe3f6a8233772357ddf854
anextin <ext_arda@hotmail.com>
2019-02-09 10:38:49 +0300

    asd
    asd

---
+++ MyApplication/app/src/main/java/com/example/ext/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.ext.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.*;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        char[] array = {1,2,3};
+        var a = array.toString();
+    }
+
+
+
+}
