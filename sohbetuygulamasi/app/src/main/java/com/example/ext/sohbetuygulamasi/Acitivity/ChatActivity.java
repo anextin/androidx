43878e0d5b4359173bd608a4b72352e32146b80a
anextin <ext_arda@hotmail.com>
2019-04-10 22:55:19 +0300

    tamam ev

--- sohbetuygulamasi/app/src/main/java/com/example/ext/sohbetuygulamasi/Acitivity/ChatActivity.java
+++ sohbetuygulamasi/app/src/main/java/com/example/ext/sohbetuygulamasi/Acitivity/ChatActivity.java
@@ -5,21 +5,31 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.ext.sohbetuygulamasi.Adapters.MessageAdapter;
+import com.example.ext.sohbetuygulamasi.Models.MessageModel;
 import com.example.ext.sohbetuygulamasi.R;
 import com.example.ext.sohbetuygulamasi.Utils.GetDate;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class   ChatActivity extends AppCompatActivity {
@@ -31,7 +41,10 @@ public class   ChatActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FloatingActionButton sendMessageButton;
     EditText mesajTextEdittext;
-
+    List<MessageModel> messageModelList;
+    RecyclerView chat_recycler_view;
+    MessageAdapter messageAdapter;
+    List<String> keyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,7 @@ public class   ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         tanimla();
         action();
+        loadMessage();
     }
 
     public String getUserName()
@@ -66,29 +80,39 @@ public class   ChatActivity extends AppCompatActivity {
         reference=firebaseDatabase.getReference();
         sendMessageButton= findViewById(R.id.sendMessageButton);
         mesajTextEdittext= findViewById(R.id.mesajTextEdittext);
+        messageModelList = new ArrayList<>();
+        keyList = new ArrayList<>();
+        chat_recycler_view=findViewById(R.id.chat_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(ChatActivity.this,1);
+        chat_recycler_view.setLayoutManager(layoutManager);
+        messageAdapter= new MessageAdapter(keyList,ChatActivity.this,ChatActivity.this,messageModelList);
+        chat_recycler_view.setAdapter(messageAdapter);
 
     }
 
     public void action()
     {
-        final String message = mesajTextEdittext.getText().toString();
-        mesajTextEdittext.setText("");
+
+
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String message = mesajTextEdittext.getText().toString();
+                mesajTextEdittext.setText("");
                 sendMessage(firebaseUser.getUid(),getId(),"text", GetDate.getDate(),false,message);
+
             }
         });
     }
 
-    public void sendMessage(final String userId, final String otherId, String textType, String date, Boolean seen, String messageText)
-    {
-        final String mesajId =reference.child("Mesajlar").child(userId).child(otherId).push().getKey();
-        final Map messageMap= new HashMap();
-        messageMap.put("type",textType);
-        messageMap.put("time",textType);
-        messageMap.put("seen",textType);
-        messageMap.put("messageText",textType);
+    public void sendMessage(final String userId, final String otherId, String textType, String date, Boolean seen, String messageText) {
+        final String mesajId = reference.child("Mesajlar").child(userId).child(otherId).push().getKey();
+        final Map messageMap = new HashMap();
+        messageMap.put("type", textType);
+        messageMap.put("time", date);
+        messageMap.put("seen", seen);
+        messageMap.put("text", messageText);
+        messageMap.put("from", userId);
 
         reference.child("Mesajlar").child(userId).child(otherId).child(mesajId).setValue(messageMap).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
@@ -102,6 +126,42 @@ public class   ChatActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void loadMessage()
+    {
+        reference.child("Mesajlar").child(firebaseUser.getUid()).child(getId()).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                MessageModel messageModel =dataSnapshot.getValue(MessageModel.class);
+                messageModelList.add(messageModel);
+                messageAdapter.notifyDataSetChanged();
+                keyList.add(dataSnapshot.getKey());
+                chat_recycler_view.scrollToPosition(messageModelList.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
 
 }
