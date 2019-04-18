acaa0f55d9fa486f886d082c04efcada6ee2f7cf
anextin <ext_arda@hotmail.com>
2019-04-18 16:42:36 +0300

    fireread

---
+++ FileRead/app/src/main/java/com/example/kafein/fileread/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.kafein.fileread;
+
+import android.content.Context;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MEDIA";
+    private TextView tv;
+    Context context;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.TextView01);
+        checkExternalMedia();
+        writeToSDFile();
+        
+
+    }
+
+    /**
+     * Method to check whether external media available and writable. This is
+     * adapted from
+     * http://developer.android.com/guide/topics/data/data-storage.html
+     * #filesExternal
+     */
+    private void checkExternalMedia() {
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // Can read and write the media
+            mExternalStorageAvailable = mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // Can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Can't read or write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }
+        tv.append("\n\nExternal Media: readable=" + mExternalStorageAvailable
+                + " writable=" + mExternalStorageWriteable);
+    }
+
+    /**
+     * Method to write ascii text characters to file on SD card. Note that you
+     * must add a WRITE_EXTERNAL_STORAGE permission to the manifest file or this
+     * method will throw a FileNotFound Exception because you won't have write
+     * permission.
+     */
+    private void writeToSDFile() {
+        // Find the root of the external storage.
+        // See http://developer.android.com/guide/topics/data/data-
+        // storage.html#filesExternal
+        File root = android.os.Environment.getExternalStorageDirectory();
+        tv.append("\nExternal file system root: " + root);
+        // See
+        // http://stackoverflow.com/questions/3551821/android-write-to-sd-card-folder
+        File dir = new File(root.getAbsolutePath() + "/Files");
+        dir.mkdirs();
+        File file = new File(dir, "myData.txt");
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            PrintWriter pw = new PrintWriter(f);
+            pw.println("Hi , How are you");
+            pw.println("Hello");
+            pw.flush();
+            pw.close();
+            f.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.i(TAG, "******* File not found. Did you"
+                    + " add a WRITE_EXTERNAL_STORAGE permission to the   manifest?");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tv.append("\n\nFile written to " + file);
+    }
+
+    /**
+     * Method to read in a text file placed in the res/raw directory of the
+     * application. The method reads in all lines of the file sequentially.
+     */
+    private void readRaw() throws IOException {
+
+        tv.append("\nData read from res/raw/textfile.txt:");
+        Context ctx = getApplicationContext();
+        String userEmalFileName = "arda";
+
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = ctx.openFileInput(userEmalFileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String lineData = bufferedReader.readLine();
+    }
+}
