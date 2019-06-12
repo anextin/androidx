b1d3314841694db35241e527955b6df4de12c82f
anextin <ext_arda@hotmail.com>
2019-06-12 14:29:32 +0300

    eczaneofis

--- nobetciezcane/app/src/main/java/com/example/ext/nobetciezcane/MainActivity.java
+++ nobetciezcane/app/src/main/java/com/example/ext/nobetciezcane/MainActivity.java
@@ -5,15 +5,18 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     String tokenText="";
     WebView webView;
+    TextView textv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +27,31 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new JsBridge(),"Android");
         this.getToken();
+        textv =findViewById(R.id.textv);
+        textv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getEczane("1");
+            }
+        });
     }
 
     public void getEczane(String id)
     {
+
+
         webView.setWebViewClient(new WebViewClient()
         {
             @Override
             public void onPageFinished(WebView view,String url)
             {
                 super.onPageFinished(view,url);
-                view.loadUrl("javascript:window.Android.htmlContentForToken(" +
+                view.loadUrl("javascript:window.Android.htmlEczaneDetay(" +
                         "'<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
             }
 
         });
-        webView.loadUrl("https://apps.istanbulsaglik.gov.tr/Eczane/nobetci?id="+id+"$token="+tokenText);
+        webView.loadUrl("https://apps.istanbulsaglik.gov.tr/Eczane/nobetci?id="+id+"&token="+tokenText);
     }
 
     public  void getToken()
@@ -56,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         }
         );
 
-        webView.loadUrl("http://apps.istanbulsaglik.gov.tr/Eczane");
+        webView.loadUrl("https://apps.istanbulsaglik.gov.tr/Eczane");
 
     }
 
@@ -67,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg)
         {
             super.handleMessage(msg);
+            Log.i("kuwai","handler");
             if(msg.what==1)
             {
                 tokenText= (String) msg.obj;
@@ -89,9 +102,11 @@ public class MainActivity extends AppCompatActivity {
             if(token.length>1)
             {
                 String token2[]=token[1].split(Pattern.quote("}"));
-                tokenText =token2[0].replaceAll(" ","")
+                tokenText =token2[0]
+                        .replaceAll(" ","")
                         .replaceAll(":","")
                         .replaceAll("\"","");
+                    Log.i("cukubik",tokenText);
                 Message message= new Message();
                 message.what=1;
                 message.obj=tokenText;
@@ -102,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
         @JavascriptInterface
         public void htmlEczaneDetay(String str)
         {
+            Log.i("cevapp",str);
             Message message= new Message();
             message.what=2;
             message.obj=str;
