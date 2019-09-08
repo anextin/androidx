8d7f2d28ac4c3180f7da50422c99ca581224524f
anextin <ext_arda@hotmail.com>
2019-09-08 21:48:02 +0300

    umarım evde yaptıklarımı commitliyorumdur

--- sohbetuygulamasi/app/src/main/java/com/example/ext/sohbetuygulamasi/Adapters/UserAdapter.java
+++ sohbetuygulamasi/app/src/main/java/com/example/ext/sohbetuygulamasi/Adapters/UserAdapter.java
@@ -1,6 +1,5 @@
 package com.example.ext.sohbetuygulamasi.Adapters;
 
-import android.animation.ArgbEvaluator;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -8,12 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
-=======
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.widget.EditText;
->>>>>>> refs/remotes/origin/ardabranch
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,10 +19,7 @@ import com.example.ext.sohbetuygulamasi.Utils.ChangeFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/origin/ardabranch
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -56,12 +46,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     DatabaseReference reference;
     FirebaseAuth auth;
     FirebaseUser user;
-<<<<<<< HEAD
     String userId="0",lastMes,ccc;
 
-=======
-    String userId,lastMes;
->>>>>>> refs/remotes/origin/ardabranch
 
 
     public UserAdapter(List<String> userKeysList, Activity activity, Context context) {
@@ -85,146 +71,98 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
 
-     //   holder.usernameTextview.setText(userKeysList.get(position).toString());
-           reference.child("Kullanicilar").child(userKeysList.get(position).toString()).addValueEventListener(new ValueEventListener() {
-               @Override
-               public void onDataChange(DataSnapshot dataSnapshot) {
+        //   holder.usernameTextview.setText(userKeysList.get(position).toString());
+        reference.child("Kullanicilar").child(userKeysList.get(position).toString()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                   Kullanicilar k1= dataSnapshot.getValue(Kullanicilar.class);
-
-
-                   auth = FirebaseAuth.getInstance();
-                   user = auth.getCurrentUser();
-                   userId = user.getUid();
-<<<<<<< HEAD
-                   holder.messageImage.setVisibility(View.INVISIBLE);
-                   System.out.println("ankara: "+userId );
-                   Query fb= reference.child("Mesajlar").child(userId).child(userKeysList.get(position).toString()).orderByKey().limitToLast(1);
-                   fb.addChildEventListener(new ChildEventListener() {
-                       @Override
-                       public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                           Boolean ggg=Boolean.parseBoolean(dataSnapshot.child("seen").getValue().toString());
-                           ccc= dataSnapshot.child("text").getValue().toString();
-                           Boolean seenOrNot=Boolean.parseBoolean(dataSnapshot.child("seen").getValue().toString());
-                           lastMes= dataSnapshot.child("text").getValue().toString();
-                           //String ggg=dataSnapshot.getValue().toString();
-                           System.out.println("gebes: "+userId +"-------"+ seenOrNot+"---"+lastMes);
-                           holder.msjTextview.setText(lastMes);
-
-                           if(seenOrNot==false  )
-                           {
-                               holder.messageImage.setVisibility(View.VISIBLE);
-                               // holder.user_state_img.setImageResource(R.drawable.online_icon);
-                           }
+                Kullanicilar k1= dataSnapshot.getValue(Kullanicilar.class);
 
 
+                auth = FirebaseAuth.getInstance();
+                user = auth.getCurrentUser();
+                userId = user.getUid();
+                holder.messageImage.setVisibility(View.INVISIBLE);
+                System.out.println("ankara: "+userId );
+                Query fb= reference.child("Mesajlar").child(userId).child(userKeysList.get(position).toString()).orderByKey().limitToLast(1);
+                fb.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Boolean ggg=Boolean.parseBoolean(dataSnapshot.child("seen").getValue().toString());
+                        ccc= dataSnapshot.child("text").getValue().toString();
+                        Boolean seenOrNot=Boolean.parseBoolean(dataSnapshot.child("seen").getValue().toString());
+                        lastMes= dataSnapshot.child("text").getValue().toString();
+                        //String ggg=dataSnapshot.getValue().toString();
+                        System.out.println("gebes: "+userId +"-------"+ seenOrNot+"---"+lastMes);
+                        holder.msjTextview.setText(lastMes);
 
-                       }
-
-                       @Override
-                       public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                       }
-
-                       @Override
-                       public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                       }
-
-                       @Override
-=======
-             //      String ac=reference.child("Mesajlar").child(userId).child(userKeysList.get(position).toString()).child("-LknDWcqCk-Ca3e68ZlZ").child("text").getKey().toString();
-                   Query fb= reference.child("Mesajlar").child(userId).child(userKeysList.get(position).toString()).orderByKey().limitToLast(1);
-                   fb.addChildEventListener(new ChildEventListener() {
-                       @Override
-                       public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                           Boolean seenOrNot=Boolean.parseBoolean(dataSnapshot.child("seen").getValue().toString());
-                           lastMes= dataSnapshot.child("text").getValue().toString();
-                           //String ggg=dataSnapshot.getValue().toString();
-                           System.out.println("gebes:"+ seenOrNot+"---"+lastMes);
-                           holder.msjTextview.setText(lastMes);
-
-                           if(seenOrNot==true)
-                           {
-                               holder.messageImage.setVisibility(View.INVISIBLE);
-                              // holder.user_state_img.setImageResource(R.drawable.online_icon);
-                           }
-                           else
-                           {
-                               holder.messageImage.setVisibility(View.VISIBLE);
-                             //  holder.user_state_img.setImageResource(R.drawable.offline_icon);
-                           }
-
-                       }
-
-                       @Override
-                       public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                       }
-
-                       @Override
-                       public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                       }
-
-                       @Override
->>>>>>> refs/remotes/origin/ardabranch
-                       public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                       }
-
-                       @Override
-                       public void onCancelled(DatabaseError databaseError) {
-
-                       }
-                   });
+                        if(seenOrNot==false  )
+                        {
+                            holder.messageImage.setVisibility(View.VISIBLE);
+                            // holder.user_state_img.setImageResource(R.drawable.online_icon);
+                        }
 
 
-<<<<<<< HEAD
+
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
 
 
 
-                    Boolean arda=false;
-
-                   Boolean userState = Boolean.parseBoolean(dataSnapshot.child("state").getValue().toString());
-=======
 
 
+                Boolean arda=false;
 
-                    Boolean arda=false;
-                   Boolean userState = Boolean.parseBoolean(dataSnapshot.child("state").getValue().toString());
-
-
->>>>>>> refs/remotes/origin/ardabranch
-                       Picasso.get().load(k1.getResim()).into(holder.userimage);
-                       holder.usernameTextview.setText(k1.getIsim());
+                Boolean userState = Boolean.parseBoolean(dataSnapshot.child("state").getValue().toString());
+                Picasso.get().load(k1.getResim()).into(holder.userimage);
+                holder.usernameTextview.setText(k1.getIsim());
 
 
-                       if(userState==true)
-                       {
-                            holder.user_state_img.setImageResource(R.drawable.online_icon);
-                       }
-                       else
-                       {
-                           holder.user_state_img.setImageResource(R.drawable.offline_icon);
-                       }
+                if(userState==true)
+                {
+                    holder.user_state_img.setImageResource(R.drawable.online_icon);
+                }
+                else
+                {
+                    holder.user_state_img.setImageResource(R.drawable.offline_icon);
+                }
 
-               }
+            }
 
-               @Override
-               public void onCancelled(DatabaseError databaseError) {
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-               }
-           });
+            }
+        });
 
-           holder.userAnaLayout.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   ChangeFragment fragment= new ChangeFragment(context);
-                   fragment.changeWithParameter(new OtherProfileFragment(),userKeysList.get(position));
-               }
-           });
+        holder.userAnaLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChangeFragment fragment= new ChangeFragment(context);
+                fragment.changeWithParameter(new OtherProfileFragment(),userKeysList.get(position));
+            }
+        });
     }
 
     //adaptera olusturulucak olan listenin size ı burda olucak
@@ -238,13 +176,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder
     {
 
-<<<<<<< HEAD
         TextView usernameTextview,msjTextview;
-=======
-        TextView usernameTextview;
-        TextView msjTextview;
-        ImageView messageImage;
->>>>>>> refs/remotes/origin/ardabranch
         CircleImageView userimage,user_state_img;
         LinearLayout userAnaLayout;
         ImageView messageImage;
@@ -256,14 +188,10 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             usernameTextview= (TextView)itemView.findViewById(R.id.usernameTextview);
             msjTextview=itemView.findViewById(R.id.msjTextview);
             messageImage=itemView.findViewById(R.id.messageImage);
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/remotes/origin/ardabranch
             userimage= (CircleImageView)itemView.findViewById(R.id.userimage);
             user_state_img= (CircleImageView)itemView.findViewById(R.id.user_state_img);
             userAnaLayout=itemView.findViewById(R.id.userAnaLayout);
-
         }
     }
 }
